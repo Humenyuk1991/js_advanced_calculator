c55050d04b44bfc1baa34b4977f85ba07c5695f2
Natalia Humenyk <gumenuknatalia53@gmail.com>
2025-12-14 22:30:57 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -7,18 +7,13 @@ function makeCalculator() {
   return {
     result: 0,
 
-    operate(callback, number) {
-      this.result = callback(this.result, number);
-
-      return this;
-    },
     add(a, b) {
       return a + b;
     },
-    subtrect(a, b) {
+    subtract(a, b) {
       return a - b;
     },
-    multyply(a, b) {
+    multiply(a, b) {
       return a * b;
     },
     divide(a, b) {
@@ -26,6 +21,11 @@ function makeCalculator() {
     },
     reset() {
       this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      this.result = callback(this.result, number);
 
       return this;
     },
